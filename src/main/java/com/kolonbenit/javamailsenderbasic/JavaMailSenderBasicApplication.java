7652253db10a0f69fa632843dc36c284086e01a8
임빈영 <qlsdud960604@gmail.com>
2023-01-31 10:06:47 +0900

    프로젝트 생성 및 환경 설정

---
+++ src/main/java/com/kolonbenit/javamailsenderbasic/JavaMailSenderBasicApplication.java
@@ -0,0 +1,13 @@
+package com.kolonbenit.javamailsenderbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMailSenderBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMailSenderBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kolonbenit/javamailsenderbasic/JavaMailSenderBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kolonbenit.javamailsenderbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMailSenderBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
