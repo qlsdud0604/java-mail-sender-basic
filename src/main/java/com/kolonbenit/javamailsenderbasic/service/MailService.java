032dc2389a8aa27520235bf217c403b3a58e6f29
임빈영 <qlsdud960604@gmail.com>
2023-01-31 17:01:25 +0900

    sendMimeMail() 메서드 추가
    MIME 메시지를 전송하기 위한 sendMimeMail() 메서드 추가

--- src/main/java/com/kolonbenit/javamailsenderbasic/service/MailService.java
+++ src/main/java/com/kolonbenit/javamailsenderbasic/service/MailService.java
@@ -1,10 +1,8 @@
 package com.kolonbenit.javamailsenderbasic.service;
 
-import com.kolonbenit.javamailsenderbasic.controller.MailController;
 import com.kolonbenit.javamailsenderbasic.dto.MailDto;
+import com.kolonbenit.javamailsenderbasic.util.MailHandler;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -16,7 +14,7 @@ public class MailService {
 
     private final JavaMailSender mailSender;
 
-    public void sendMail(MailDto mailDto) {
+    public void sendTextMail(MailDto mailDto) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(mailDto.getAddress());
         message.setFrom(FROM_ADDRESS);
@@ -24,5 +22,27 @@ public class MailService {
         message.setText(mailDto.getContent());
 
         mailSender.send(message);
+    }
+
+    public void sendMimeMail(MailDto mailDto) {
+        try {
+            MailHandler mailHandler = new MailHandler(mailSender);
+            // 받는 사람
+            mailHandler.setTo(mailDto.getAddress());
+            // 보내는 사람
+            mailHandler.setFrom(FROM_ADDRESS);
+            // 제목
+            mailHandler.setSubject(mailDto.getTitle());
+            // HTML layout
+            String htmlContent = "<p>" + mailDto.getContent() +"<p> <img src='cid:test_image'>";
+            mailHandler.setText(htmlContent, true);
+            // 첨부 파일
+            mailHandler.setAttach("test.txt", "static/test.txt");
+            // 이미지 삽입
+            mailHandler.setInline("test_image", "static/test_image.jpg");
+            mailHandler.send();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
