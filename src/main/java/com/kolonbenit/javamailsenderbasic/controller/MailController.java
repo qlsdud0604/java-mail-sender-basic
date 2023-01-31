f053155d6f0405f037f068cbb695df9e8805c699
임빈영 <qlsdud960604@gmail.com>
2023-01-31 14:57:20 +0900

    MailController 클래스 생성
    사용자 요청을 처리하기 위한 Controller 생성

---
+++ src/main/java/com/kolonbenit/javamailsenderbasic/controller/MailController.java
@@ -0,0 +1,26 @@
+package com.kolonbenit.javamailsenderbasic.controller;
+
+import com.kolonbenit.javamailsenderbasic.dto.MailDto;
+import com.kolonbenit.javamailsenderbasic.service.MailService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MailController {
+    private final MailService mailService;
+
+    @GetMapping("/mail")
+    public String writeMail() {
+        return "mail";
+    }
+
+    @PostMapping("/mail")
+    public void sendMail(MailDto mailDto) {
+        mailService.sendMail(mailDto);
+    }
+}
